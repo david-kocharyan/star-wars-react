97dd2c815608ae1ad5391593f22f75227ef179ee
David <david@aimtech.am>
2020-12-14 10:44:56 +0400

    first commit

---
+++ src/components/random-planet/random-planet.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import SwapiService from "../../services/swapi-service";
+import Spinner from "../spinner/spinner";
+import PlanetView from "./planet-view";
+
+import './random-planet.css'
+import ErrorIndicator from "../error-indicator/error-indicator";
+
+
+class RandomPlanet extends Component {
+    swapiService = new SwapiService();
+
+    state = {
+        planet: {},
+        loading: true,
+        error: false,
+    };
+
+    constructor() {
+        super();
+        this.updatePlanet()
+    }
+
+    onPlanetLoaded = (planet) => {
+        this.setState({
+            planet,
+            loading: false,
+        });
+    };
+
+    onError = (err) => {
+        this.setState({
+            error: true,
+            loading: false,
+        })
+    };
+
+    updatePlanet() {
+        const id = Math.floor(Math.random() * 20) + 2;
+        this.swapiService
+            .getPlanet(id)
+            .then(this.onPlanetLoaded)
+            .catch(this.onError);
+    }
+
+    render() {
+        const {planet, loading, error} = this.state;
+
+        const errorMessage = error ? <ErrorIndicator/> : null
+        const spinner = loading ? <Spinner/> : null;
+        const content = !loading && !error  ? <PlanetView planet={planet}/> : null;
+
+        return (
+            <div className="random-planet jumbotron rounded">
+                {errorMessage}
+                {spinner}
+                {content}
+            </div>
+        )
+    }
+}
+
+export default RandomPlanet;
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-nzz1a2fs66c">
+            <div className="ldio-xl7w9y0kufl">
+                <div></div>
+                <div></div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Spinner;
